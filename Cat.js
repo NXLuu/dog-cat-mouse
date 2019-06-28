461bcc354f71c301f6218f8fdaef427b2e747396
LuuNguyen <luunguyen16520@gmail.com>
2019-06-28 17:25:58 +0700

    Let Dog eat Cat

--- Cat.js
+++ Cat.js
@@ -1,11 +1,14 @@
 function Cat(name) {
 	this.name = name;
-	this.die = true;
+	this.isDie = false;
 	this.stomache = [];
 };
 
 Cat.prototype.eat = function(mouse) {
 	this.stomache.push(mouse);
 }
+Cat.prototype.die = function() {
+	this.isDie = true;
+};
 
 module.exports = Cat;
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var Dog = require('./Dog');
+var Cat = require('./Cat');
 
 var dog = new Dog('Tom');
+var cat = new Cat('Jerry');
+console.log(cat);
 dog.sayHi();
+cat.die();
+dog.eat(cat);
+
+console.log(dog);
