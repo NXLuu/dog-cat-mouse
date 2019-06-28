ea3f56f6f447aa20d542d2ccf72e97d2d5b65b34
LuuNguyen <luunguyen16520@gmail.com>
2019-06-28 17:05:30 +0700

    Using ignore and Chalk npm in Dog module

--- Dog.js
+++ Dog.js
@@ -1,8 +1,15 @@
-function Dog() {
+var chalk = require('chalk');
+function Dog(name) {
 	this.stomach = [];
-
+	this.name = name;
 };
 
 Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
 }
+
+Dog.prototype.sayHi = function() {
+	console.log('Hi, i am a dog. This is my name:'+ chalk.blue(this.name));
+}
+
+module.exports = Dog;
